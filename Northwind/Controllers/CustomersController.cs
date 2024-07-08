c5cd83f509f1804f1d43b72c776971a6739b11c0
souvikk27 <souvikvip@outlook.com>
2024-07-08 13:31:10 +0530

    added customers controller

---
+++ Northwind/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Context;
+
+namespace Northwind.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _context.Customers.AsNoTracking().ToList();
+            return View(customers);
+        }
+    }
+}
