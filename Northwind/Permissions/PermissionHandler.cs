70db2eb3f2c9ac088d30a15133c5dd4fb80bef58
Souvik Kundu <souvikvip@outlook.com>
2024-07-10 18:26:27 +0530

    base implementation of permission rquirement and permission handler

--- Northwind/Permissions/PermissionHandler.cs
+++ Northwind/Permissions/PermissionHandler.cs
@@ -4,9 +4,20 @@ namespace Northwind.Permissions
 {
     public class PermissionHandler : AuthorizationHandler<PermissionRequirement>
     {
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+            PermissionRequirement requirement)
         {
-            throw new NotImplementedException();
+            var userClaims = context.User.Claims;
+
+            // Check if the user has a permission claim for the controller and any of the actions
+            if (requirement.ActionNames.Any(action => userClaims.Any(c => c.Type == "Permission" &&
+                                                                          c.Value == $"{requirement.ControllerName}.{action}")))
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
--- Northwind/Permissions/PermissionRequirement.cs
+++ Northwind/Permissions/PermissionRequirement.cs
@@ -4,5 +4,13 @@ namespace Northwind.Permissions
 {
     public class PermissionRequirement : IAuthorizationRequirement
     {
+        public string ControllerName { get; }
+        public List<string> ActionNames { get; }
+
+        public PermissionRequirement(string controllerName, List<string> actionNames)
+        {
+            ControllerName = controllerName;
+            ActionNames = actionNames ?? new List<string>();
+        }
     }
 }
