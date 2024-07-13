990d6afd5c4801c718a60809c6446f90bc5c5324
souvikk27 <souvikvip@outlook.com>
2024-07-13 19:34:25 +0530

    updated modifications

--- Northwind/Services/Railway/Result.cs
+++ Northwind/Services/Railway/Result.cs
@@ -21,9 +21,10 @@ namespace Northwind.Services.Railway
 
         public Result<TNew> Bind<TNew>(Func<T, Result<TNew>> func) =>
             IsSuccess ? func(Value) : Result<TNew>.Failure(Error);
-        
+
         public async Task<Result<TNew>> MapAsync<TNew>(Func<T, Task<TNew>> func) =>
             IsSuccess ? Result<TNew>.Success(await func(Value)) : Result<TNew>.Failure(Error);
+
         public async Task<Result<TNew>> BindAsync<TNew>(Func<T, Task<Result<TNew>>> func) =>
             IsSuccess ? await func(Value) : Result<TNew>.Failure(Error);
     }
