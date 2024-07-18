4e8dc8c0cdccbc75c0d4382841e86723179f4459
souvikk27 <souvikvip@outlook.com>
2024-07-18 22:29:16 +0530

    cross cutting concerns

---
+++ Northwind/Extensions/AuthorizationExtension.cs
@@ -0,0 +1,51 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Permissions;
+
+namespace Northwind.Extensions;
+
+public static class AuthorizationExtension
+{
+    public static void ConfigureAuthorizationPolicy(this IServiceCollection services)
+    {
+        services.AddAuthorization(options =>
+        {
+            // Get all controller types in the assembly
+            var controllers = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(type => typeof(ControllerBase).IsAssignableFrom(type))
+                .ToList();
+
+            foreach (var policyName in from controller in controllers
+                     let controllerName = controller.Name.Replace("Controller", "")
+                     let actions = controller.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                         .Where(m =>
+                             (typeof(IActionResult).IsAssignableFrom(m.ReturnType) ||
+                              typeof(Task<IActionResult>).IsAssignableFrom(m.ReturnType)) &&
+                             m.DeclaringType == controller)
+                         .Select(m => m.Name)
+                         .ToList()
+                     from action in actions
+                     let actionName = action
+                     select $"{controllerName}-{actionName}")
+            {
+                options.AddPolicy(policyName, policy =>
+                    policy.RequireClaim("Permission", policyName));
+            }
+
+            options.AddPolicy("RoleResourcePolicy", policy =>
+            {
+                policy.RequireAssertion(context =>
+                    context.Requirements.Any(_ =>
+                        context.User.HasClaim(c =>
+                            c is { Type: "Permission", Value: $"Roles-Manage" or $"Resources-Scopes" }
+                        )
+                    )
+                );
+            });
+        });
+
+        services.AddSingleton<IAuthorizationHandler, PermissionHandler>();
+        services.AddSingleton<IAuthorizationHandler, RoleResourceAuthRequirementHandler>();
+    }
+}
--- Northwind/Extensions/ServiceExtensions.cs
+++ Northwind/Extensions/ServiceExtensions.cs
@@ -31,47 +31,4 @@ public static class ServiceExtensions
         services.AddScoped<IRoleService, RoleService>();
         services.AddScoped<IResourceService, ResourceService>();
     }
-
-
-    public static void ConfigureAuthorizationPolicy(this IServiceCollection services)
-    {
-        services.AddAuthorization(options =>
-        {
-            // Get all controller types in the assembly
-            var controllers = Assembly.GetExecutingAssembly().GetTypes()
-                .Where(type => typeof(ControllerBase).IsAssignableFrom(type))
-                .ToList();
-
-            foreach (var policyName in from controller in controllers
-                     let controllerName = controller.Name.Replace("Controller", "")
-                     let actions = controller.GetMethods(BindingFlags.Public | BindingFlags.Instance)
-                         .Where(m =>
-                             (typeof(IActionResult).IsAssignableFrom(m.ReturnType) ||
-                              typeof(Task<IActionResult>).IsAssignableFrom(m.ReturnType)) &&
-                             m.DeclaringType == controller)
-                         .Select(m => m.Name)
-                         .ToList()
-                     from action in actions
-                     let actionName = action
-                     select $"{controllerName}-{actionName}")
-            {
-                options.AddPolicy(policyName, policy =>
-                    policy.RequireClaim("Permission", policyName));
-            }
-
-            options.AddPolicy("RoleResourcePolicy", policy =>
-            {
-                policy.RequireAssertion(context =>
-                    context.Requirements.Any(r =>
-                        context.User.HasClaim(c =>
-                            c is { Type: "Permission", Value: $"Roles-Manage" or $"Resources-Scopes" }
-                        )
-                    )
-                );
-            });
-        });
-
-        services.AddSingleton<IAuthorizationHandler, PermissionHandler>();
-        services.AddSingleton<IAuthorizationHandler, RoleResourceAuthRequirementHandler>();
-    }
 }
