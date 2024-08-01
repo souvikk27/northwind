4ebd1c920ff3d0602fd77a3f848fa8822cb87a8b
souvikk27 <souvikvip@outlook.com>
2024-08-01 15:12:06 +0530

    added flowbite ui components

--- Northwind/tailwind.config.js
+++ Northwind/tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './Pages/**/*.cshtml',
-    './Views/**/*.cshtml'
+        './Pages/**/*.cshtml',
+        './Views/**/*.cshtml',
+        "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('flowbite/plugin')
   ],
 }
 
